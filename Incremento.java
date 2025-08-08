eb45b644ab62d20d4f718fe7d8116fd63a08c07b
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-08-08 09:04:13 -0300

    Add files via upload

---
+++ Incremento.java
@@ -0,0 +1,18 @@
+    /*
+Colégio Antonio de Moraes Barros
+Prof: Edson
+Disciplina: Back-End
+ */
+// Declaração da classe chamada OlaMundo
+public class incremento {
+
+    // Método principal: é o ponto de entrada do programa Java
+    public static void main(String[] args) {
+
+        int numero  = 1;
+
+        System.out.println(++numero);
+
+    }
+
+} // fim da classe
